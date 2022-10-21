1a92619b6f8fcaf9b4df9f12d24caf1ed1820650
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-10-22 00:04:50 +0100

    Added test to check nav elements

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -6,4 +6,19 @@ describe("MainNav", () => {
     const wrapper = mount(MainNav);
     expect(wrapper.text()).toMatch("Ship Happens");
   });
+  it("displays menu items for navigation", () => {
+    const wrapper = mount(MainNav);
+    const navigationMenuItems = wrapper.findAll(
+      "[data-test='main-nav-list-item']"
+    );
+    const navigationMenuText = navigationMenuItems.map((item) => item.text());
+    expect(navigationMenuText).toEqual([
+      "Teams",
+      "Locations",
+      "Life at Ship Happens",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
+  });
 });
