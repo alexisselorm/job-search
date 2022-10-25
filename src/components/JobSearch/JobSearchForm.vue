1faca665c32cb7989b3a4e4da0038487f13344bb
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-10-26 00:32:48 +0100

    Text input extracted into a reusable component

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -1,18 +1,13 @@
 <template>
   <form
-    class="flex items-center w-full h-12 mt-14 border border-solid border-brand-gray-3 rounded-3xl"
+    class="flex items-center w-full h-12 border border-solid mt-14 border-brand-gray-3 rounded-3xl"
     action=""
   >
     <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
-    <div class="flex flex-nowrap flex-1 h-full text-base font-light">
+    <div class="flex flex-1 h-full text-base font-light flex-nowrap">
       <div class="relative flex items-center flex-1 h-full pr-3">
         <label class="absolute left-0 -top-10" for="role">Role</label>
-        <input
-          v-model="role"
-          type="text"
-          placeholder="Software Engineer"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <TextInput placeholder="Software Engineer" />
       </div>
       <span
         class="flex items-center h-full px-3 border-l border-r border-brand-gray-3 bg-brand-gray-2"
@@ -34,10 +29,12 @@
 
 <script>
 import ActionButton from "../Shared/ActionButton.vue";
+import TextInput from "@/components/Shared/TextInput.vue";
 export default {
   name: "JobSearchForm",
   components: {
     ActionButton,
+    TextInput,
   },
   data() {
     return {
---
+++ src/components/Shared/TextInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <input
+    :value="value"
+    class="w-full text-lg font-normal focus:outline-none"
+    :placeholder="placeholder"
+    type="text"
+    @input="handleInput"
+  />
+</template>
+
+<script>
+export default {
+  name: "TextInput",
+  props: {
+    placeholder: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      value: "",
+    };
+  },
+  methods: {
+    handleInput($event) {
+      this.value = $event.target.value;
+    },
+  },
+};
+</script>
