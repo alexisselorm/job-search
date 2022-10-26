5e12ddb3a629db399b12fb8fb69aa4c1f1d0449f
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-10-26 03:56:58 +0100

    Vue router

---
+++ src/views/AboutView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1 class="text-6xl font-bold">This is an about page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutView",
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="text-6xl font-bold">Home View</h1>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "HomeView",
+};
+</script>
