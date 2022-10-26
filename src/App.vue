0b4419d72b8f560b6ddb4a4b78332b6ef9c06bb2
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-10-26 11:11:56 +0100

    Router link

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/jobs/results">Job Results</router-link>
+  </div>
   <router-view />
 </template>
 
