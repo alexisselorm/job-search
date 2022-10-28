2bd70a92e47a86c08db117c0e54b793c537b084c
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-10-28 20:11:25 +0100

    More tests for job listing

--- tests/unit/components/JobResults/JobListing.test.js
+++ tests/unit/components/JobResults/JobListing.test.js
@@ -1,4 +1,4 @@
-import { mount } from "@vue/test-utils";
+import { mount, RouterLinkStub } from "@vue/test-utils";
 
 import JobListing from "@/components/JobResults/JobListing";
 import jobListingStub from "@/utils/jobListingStub";
@@ -20,5 +20,30 @@ describe("JobListing", () => {
       jobListingStub({ organization: "Ship Happens" })
     );
     expect(wrapper.text()).toMatch("Ship Happens");
+  });
+
+  it("renders job location", () => {
+    const wrapper = mount(
+      JobListing,
+      jobListingStub({ locations: ["Stockholm", "Paris"] })
+    );
+    expect(wrapper.text()).toMatch("Stockholm");
+    expect(wrapper.text()).toMatch("Paris");
+  });
+
+  it("renders job qualifications", () => {
+    const wrapper = mount(
+      JobListing,
+      jobListingStub({ minimumQualifications: ["Code", "Develop"] })
+    );
+    expect(wrapper.text()).toMatch("Code");
+    expect(wrapper.text()).toMatch("Develop");
+  });
+
+  it("links to job's page", () => {
+    const wrapper = mount(JobListing, jobListingStub({ id: 1 }));
+    const jobPageLink = wrapper.findComponent(RouterLinkStub);
+    const toProp = jobPageLink.props("to");
+    expect(toProp).toBe("/jobs/results/1");
   });
 });
