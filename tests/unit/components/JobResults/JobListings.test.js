d5fecc85209a1f0494f514daa3101ed539b84989
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-10-28 21:44:09 +0100

    Job listings tests fail. Need fixing

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -1,9 +1,23 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, flushPromises } from "@vue/test-utils";
 import axios from "axios";
-import JobListings from "@/components/JobResults/JobListings";
 jest.mock("axios");
 
+import JobListings from "@/components/JobResults/JobListings";
+
 describe("JobListings", () => {
-  shallowMount(JobListings);
-  expect(axios.get).toHaveBeenCalled("http://localhost:3000/jobs");
+  it("fetches jobs", async () => {
+    shallowMount(JobListings);
+    await flushPromises();
+    axios.get.mockResolvedValue({ data: [] });
+    console.log(axios.get());
+    expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
+  });
+
+  it("creates a job listing for each received job", async () => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+    const wrapper = shallowMount(JobListings);
+    await flushPromises();
+    const jobListings = wrapper.findAll("[data-test='job-listing']");
+    expect(jobListings.length).toHaveLength(15);
+  });
 });
