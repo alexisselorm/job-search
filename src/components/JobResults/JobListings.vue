87d4d4b5231d6ee37c65030af1a84e42a3df395a
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-10-28 15:40:03 +0100

    Job listing component done

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -1,9 +1,19 @@
 <template>
-  <main class="flex-auto p-8 bg-brand-gray-2">Job listings</main>
+  <main class="flex-auto p-8 bg-brand-gray-2">
+    <ol>
+      <job-listing />
+      <job-listing />
+      <job-listing />
+    </ol>
+  </main>
 </template>
 
 <script>
+import JobListing from "./JobListing.vue";
 export default {
   name: "JobListings",
+  components: {
+    JobListing,
+  },
 };
 </script>
