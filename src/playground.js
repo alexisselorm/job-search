6df3be49dc5909f7aeee289d37e483c67d6d79e4
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-10-30 06:09:56 +0000

    Dynamic pagination

--- src/playground.js
+++ src/playground.js
@@ -13,20 +13,32 @@
 // console.log(list.length);
 // console.log(0 % 4);
 
-const axios = require("axios");
+// const axios = require("axios");
 
-const url = "http://localhost:3000/jobs";
-const fetchJobsv1 = () => {
-  axios
-    .get(url)
-    .then((response) => console.log(response))
-    .catch((reject) => console.log(reject.message));
-};
-fetchJobsv1();
+// const url = "http://localhost:3000/jobs";
+// const fetchJobsv1 = () => {
+//   axios
+//     .get(url)
+//     .then((response) => console.log(response))
+//     .catch((reject) => console.log(reject.message));
+// };
+// fetchJobsv1();
 
-const fetchJobsv2 = async () => {
-  const result = await axios.get(url);
-  console.log(result.data);
-};
+// const fetchJobsv2 = async () => {
+//   const result = await axios.get(url);
+//   console.log(result.data);
+// };
 
-fetchJobsv2();
+// fetchJobsv2();
+
+// const sushi = [
+//   "Tuna",
+//   "Salmon",
+//   "Shrimp",
+//   "Uni",
+//   "YellowTail",
+//   "Lavender",
+//   "Seshimi",
+// ];
+
+// console.log(sushi.slice(2, 8));
