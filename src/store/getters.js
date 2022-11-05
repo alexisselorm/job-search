d1cc3fb807752935ae3c483fa32a301b28a336e8
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-11-05 17:38:54 +0000

    Fixed matched job count in subnav

--- src/store/getters.js
+++ src/store/getters.js
@@ -46,18 +46,10 @@ const getters = {
     if (state.selectedJobTypes.length === 0) return true;
     return state.selectedJobTypes.includes(job.jobType);
   },
-  [FILTERED_JOBS](state) {
-    const noSelectedOrganizations = state.selectedOrganizations.length === 0;
-    const noSelectedJobTypes = state.selectedJobTypes.length === 0;
+  [FILTERED_JOBS](state, getters) {
     return state.jobs
-      .filter((job) => {
-        if (noSelectedOrganizations) return true;
-        return state.selectedOrganizations.includes(job.organization);
-      })
-      .filter((job) => {
-        if (noSelectedJobTypes) return true;
-        return state.selectedJobTypes.includes(job.jobType);
-      });
+      .filter((job) => getters.INCLUDE_JOB_BY_ORGANIZATION(job))
+      .filter((job) => getters.INCLUDE_JOB_BY_JOB_TYPE(job));
   },
 };
 
