0e03e90e05f86dfbcb8c86209fc7e3c3af94c9fd
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-11-06 22:51:35 +0000

    Merged the tests for previous side bar components

--- tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.test.js
+++ tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.test.js
@@ -1,5 +1,5 @@
 import { mount } from "@vue/test-utils";
-import JobFiltersSidebarJobTypes from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarJobTypes";
+import JobFiltersSidebarCheckboxGroup from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup";
 
 import { useStore } from "vuex";
 jest.mock("vuex");
@@ -9,48 +9,70 @@ jest.mock("vue-router");
 
 import { useUniqueJobTypes } from "@/store/composables";
 jest.mock("@/store/composables");
-describe("JobFiltersSidebarJobTypes", () => {
-  const createConfig = () => ({
+
+describe("JobFiltersSidebarCheckboxGroup", () => {
+  const createConfig = (props = {}) => ({
     global: {
       stubs: {
         FontAwesomeIcon: true,
       },
     },
+    props: {
+      header: "Some header",
+      uniqueValues: new Set(["Value A", "Value B"]),
+      mutation: "some mutation",
+      ...props,
+    },
   });
   it("renders a unique list of job types for filtering jobs", async () => {
-    useUniqueJobTypes.mockReturnValue(new Set(["Intern", "Full-time"]));
+    useUniqueJobTypes.mockReturnValue(new Set(["Value A", "Value B"]));
 
-    const wrapper = mount(JobFiltersSidebarJobTypes, createConfig());
+    const props = { uniqueValues: new Set(["Value A", "Value B"]) };
+    const wrapper = mount(JobFiltersSidebarCheckboxGroup, createConfig(props));
 
     const clickableArea = wrapper.find("[data-test='clickable-area']");
     await clickableArea.trigger("click");
-    const jobTypeLabels = wrapper.findAll("[data-test='job-type']");
-    const jobTypes = jobTypeLabels.map((node) => node.text());
-    expect(jobTypes).toEqual(["Intern", "Full-time"]);
+    const inputLabels = wrapper.findAll("[data-test='value']");
+    const inputs = inputLabels.map((node) => node.text());
+    expect(inputs).toEqual(["Value A", "Value B"]);
   });
 
   describe("when user clicks checkbox", () => {
-    it("communicates that user has selected checkbox for job types", async () => {
+    it("communicates that user has selected checkbox for value", async () => {
       useUniqueJobTypes.mockReturnValue(new Set(["Intern", "Full-time"]));
       const commit = jest.fn();
+      const props = {
+        mutation: "SOME_MUTATION",
+        uniqueValues: new Set(["Full-time"]),
+        commit,
+      };
       useStore.mockReturnValue({ commit });
       useRouter.mockReturnValue({ push: jest.fn() });
-      const wrapper = mount(JobFiltersSidebarJobTypes, createConfig());
+      const wrapper = mount(
+        JobFiltersSidebarCheckboxGroup,
+        createConfig(props)
+      );
       const clickableArea = wrapper.find("[data-test='clickable-area']");
       await clickableArea.trigger("click");
-      const internInput = wrapper.find("[data-test='Intern']");
+      const internInput = wrapper.find("[data-test='Full-time']");
       await internInput.setChecked();
-      expect(commit).toHaveBeenCalledWith("ADD_SELECTED_JOB_TYPES", ["Intern"]);
+      expect(commit).toHaveBeenCalledWith("SOME_MUTATION", ["Full-time"]);
     });
     it("navigates user to job page for fresh job results", async () => {
       useUniqueJobTypes.mockReturnValue(new Set(["Intern", "Full-time"]));
       useStore.mockReturnValue({ commit: jest.fn() });
       const push = jest.fn();
       useRouter.mockReturnValue({ push });
-      const wrapper = mount(JobFiltersSidebarJobTypes, createConfig());
+      const props = {
+        uniqueValues: new Set(["Full-time"]),
+      };
+      const wrapper = mount(
+        JobFiltersSidebarCheckboxGroup,
+        createConfig(props)
+      );
       const clickableArea = wrapper.find("[data-test='clickable-area']");
       await clickableArea.trigger("click");
-      const internInput = wrapper.find("[data-test='Intern']");
+      const internInput = wrapper.find("[data-test='Full-time']");
       await internInput.setChecked();
       expect(push).toHaveBeenCalledWith({ name: "JobResults" });
     });
