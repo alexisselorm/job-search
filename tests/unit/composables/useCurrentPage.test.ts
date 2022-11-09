de725ba09463c0464e811eb85444ccc3c69c6142
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-11-09 23:40:09 +0000

    Refactored useCurrentPage composable to typescript

--- tests/unit/composables/useCurrentPage.test.ts
+++ tests/unit/composables/useCurrentPage.test.ts
@@ -1,18 +1,19 @@
 import useCurrentPage from "@/composables/useCurrentPage";
 import { useRoute } from "vue-router";
 jest.mock("vue-router");
+const useRouteMock = useRoute as jest.Mock;
 
 describe("useCurrentPage", () => {
   describe("when query params include page", () => {
     it("returns page", () => {
-      useRoute.mockReturnValue({ query: { page: "5" } });
+      useRouteMock.mockReturnValue({ query: { page: "5" } });
       const result = useCurrentPage();
       expect(result.value).toBe(5);
     });
   });
   describe("when query params excludes page", () => {
     it("defaults to page 1", () => {
-      useRoute.mockReturnValue({ query: {} });
+      useRouteMock.mockReturnValue({ query: {} });
       const result = useCurrentPage();
       expect(result.value).toBe(1);
     });
