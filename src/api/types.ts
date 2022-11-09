4e52021ca89aae84fa8845a94d1e697560725f5c
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-11-09 07:14:29 +0000

    Added jobInterface and a GlobalState interface

---
+++ src/api/types.ts
@@ -0,0 +1,12 @@
+export interface Job {
+  id: string;
+  title: string;
+  organization: string;
+  degree: string;
+  jobType: string;
+  locations: string[];
+  minimumQualifications: string[];
+  preferredQualifications: string[];
+  description: string[];
+  dateAdded: string;
+}
--- src/store/state.ts
+++ src/store/state.ts
@@ -1,4 +1,7 @@
-const state = () => {
+import { Job } from "@/api/types";
+import { GlobalState } from "@/store/types";
+
+const state = (): GlobalState => {
   return {
     isLoggedIn: false,
     jobs: [],
