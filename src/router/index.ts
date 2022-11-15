9e459aa7dcb06d2a6ef52c5cee1dbb9480b05b80
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-11-15 15:35:38 +0000

    Refactored Router file to typescript

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 // Lazy load components
-const HomeView = () => import("@/views/HomeView");
+const HomeView = () => import("@/views/HomeView.vue");
 const JobResultsView = () =>
-  import(/*webpackChunkName:"jobs"*/ "@/views/JobResultsView");
-const JobView = () => import(/*webpackChunkName:"jobs"*/ "@/views/JobView");
+  import(/*webpackChunkName:"jobs"*/ "@/views/JobResultsView.vue");
+const JobView = () => import(/*webpackChunkName:"jobs"*/ "@/views/JobView.vue");
 const TeamsView = () =>
-  import(/*webpackChunkName:"teams"*/ "@/views/TeamsView");
+  import(/*webpackChunkName:"teams"*/ "@/views/TeamsView.vue");
 
 const routes = [
   { path: "/", name: "home", component: HomeView },
