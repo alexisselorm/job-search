585de586f87e1308632f310ddba3517ec3bd790b
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-11-16 17:00:10 +0000

    Wrote tests for extracted filters sidebar degrees component

---
+++ tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebarDegrees.test.ts
@@ -0,0 +1,20 @@
+import { shallowMount } from "@vue/test-utils";
+jest.mock("vuex");
+import { useUniqueDegrees } from "@/store/composables";
+jest.mock("@/store/composables");
+const useUniqueDegreesMock = useUniqueDegrees as jest.Mock;
+
+import JobFiltersSidebarDegrees from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarDegrees.vue";
+
+describe("JobFiltersSidebarDegrees", () => {
+  it("allows users to filter  by degree", () => {
+    useUniqueDegreesMock.mockReturnValue(new Set(["AirBnB", "Google"]));
+    const wrapper = shallowMount(JobFiltersSidebarDegrees);
+    const DegreesFilter = wrapper.findComponent({
+      name: "JobFiltersSidebarCheckboxGroup",
+    });
+    const { uniqueValues, mutation } = DegreesFilter.props();
+    expect(uniqueValues).toEqual(new Set(["AirBnB", "Google"]));
+    expect(mutation).toBe("ADD_SELECTED_DEGREES");
+  });
+});
