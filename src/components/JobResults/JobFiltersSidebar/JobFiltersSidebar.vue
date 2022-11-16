ca7c747342bcab5f3d63f5efed32104decc58555
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-11-16 10:36:51 +0000

    Mutation subscriptions added to clear checkboxes when filters are cleared

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -6,7 +6,11 @@
       <div class="flex flex-row justify between">
         <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
         <div class="flex items-center text-sm">
-          <ActionButton text="Clear Filters" type="secondary" />
+          <ActionButton
+            text="Clear Filters"
+            type="secondary"
+            @click="clearUserJobFilterSelections"
+          />
         </div>
       </div>
       <!-- <Accordion header="Degree"> -->
@@ -37,6 +41,8 @@
 </template>
 <script lang="ts">
 import ActionButton from "@/components/Shared/ActionButton.vue";
+import { key } from "@/store";
+import { useStore } from "vuex";
 // import Accordion from "@/components/Shared/Accordion.vue";
 import JobFiltersSidebarCheckboxGroup from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.vue";
 
@@ -49,6 +55,7 @@ import {
   ADD_SELECTED_JOB_TYPES,
   ADD_SELECTED_ORGANIZATIONS,
   ADD_SELECTED_DEGREES,
+  CLEAR_USER_JOB_FILTER_SELECTIONS,
 } from "@/store/constants";
 import { defineComponent } from "vue";
 export default defineComponent({
@@ -59,13 +66,18 @@ export default defineComponent({
     JobFiltersSidebarCheckboxGroup,
   },
   setup() {
+    const store = useStore(key);
     const uniqueOrganizations = useUniqueOrganizations();
     const uniqueJobTypes = useUniqueJobTypes();
     const uniqueDegrees = useUniqueDegrees();
+    const clearUserJobFilterSelections = () => {
+      store.commit(CLEAR_USER_JOB_FILTER_SELECTIONS);
+    };
     return {
       uniqueOrganizations,
       uniqueJobTypes,
       uniqueDegrees,
+      clearUserJobFilterSelections,
       ADD_SELECTED_JOB_TYPES,
       ADD_SELECTED_DEGREES,
       ADD_SELECTED_ORGANIZATIONS,
--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.vue
@@ -27,6 +27,7 @@ import { defineComponent, ref, PropType } from "vue";
 import { useStore } from "vuex";
 import { key } from "@/store/index";
 import { useRouter } from "vue-router";
+import { CLEAR_USER_JOB_FILTER_SELECTIONS } from "@/store/constants";
 export default defineComponent({
   name: "JobFiltersSidebarCheckboxGroup",
   components: {
@@ -49,7 +50,15 @@ export default defineComponent({
   setup(props) {
     const router = useRouter();
     const store = useStore(key);
+
     const selectedValues = ref<string[]>([]);
+    // Pay attention to our  clear user job filter selection mutation and clear our checkboxes when this mutation runs
+    store.subscribe((mutation) => {
+      if (mutation.type === CLEAR_USER_JOB_FILTER_SELECTIONS) {
+        selectedValues.value = [];
+      }
+    });
+
     const selectValue = () => {
       store.commit(props.mutation, selectedValues.value);
       router.push({ name: "JobResults" });
