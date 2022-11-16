df4c5767a5271a78e52950de9e2e9cf5b1ff1502
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-11-16 16:25:46 +0000

    Extracted job types out of filters sidebar

---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarJobTypes.vue
@@ -0,0 +1,25 @@
+<template>
+  <JobFiltersSidebarCheckboxGroup
+    :unique-values="uniqueJobTypes"
+    :mutation="ADD_SELECTED_JOB_TYPES"
+    data-test="job-types-filter"
+  />
+</template>
+<script lang="ts">
+import { ADD_SELECTED_JOB_TYPES } from "@/store/constants";
+import { useUniqueJobTypes } from "@/store/composables";
+import JobFiltersSidebarCheckboxGroup from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.vue";
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "JobFiltersSidebarDegrees",
+  components: {
+    JobFiltersSidebarCheckboxGroup,
+  },
+
+  setup() {
+    const uniqueJobTypes = useUniqueJobTypes();
+
+    return { uniqueJobTypes, ADD_SELECTED_JOB_TYPES };
+  },
+});
+</script>
