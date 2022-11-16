b8682e30a1d8609564e75a4c407ab59ed0d61d70
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-11-16 16:27:28 +0000

    Extracted organizations out of filters sidebar

---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -0,0 +1,25 @@
+<template>
+  <JobFiltersSidebarCheckboxGroup
+    :unique-values="uniqueOrganizations"
+    :mutation="ADD_SELECTED_ORGANIZATIONS"
+    data-test="organiztions-filter"
+  />
+</template>
+<script lang="ts">
+import { ADD_SELECTED_ORGANIZATIONS } from "@/store/constants";
+import { useUniqueOrganizations } from "@/store/composables";
+import JobFiltersSidebarCheckboxGroup from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.vue";
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "JobFiltersSidebarDegrees",
+  components: {
+    JobFiltersSidebarCheckboxGroup,
+  },
+
+  setup() {
+    const uniqueOrganizations = useUniqueOrganizations();
+
+    return { uniqueOrganizations, ADD_SELECTED_ORGANIZATIONS };
+  },
+});
+</script>
