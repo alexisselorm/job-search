71b3fb2957b683379955f70baebf3f09796e0cb4
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-11-16 16:22:41 +0000

    Extracted degrees out of filters sidebar

---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarDegrees.vue
@@ -0,0 +1,25 @@
+<template>
+  <JobFiltersSidebarCheckboxGroup
+    :unique-values="uniqueDegrees"
+    :mutation="ADD_SELECTED_DEGREES"
+    data-test="degrees-filter"
+  />
+</template>
+<script lang="ts">
+import { ADD_SELECTED_DEGREES } from "@/store/constants";
+import { useUniqueDegrees } from "@/store/composables";
+import JobFiltersSidebarCheckboxGroup from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.vue";
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "JobFiltersSidebarDegrees",
+  components: {
+    JobFiltersSidebarCheckboxGroup,
+  },
+
+  setup() {
+    const uniqueDegrees = useUniqueDegrees();
+
+    return { uniqueDegrees, ADD_SELECTED_DEGREES };
+  },
+});
+</script>
