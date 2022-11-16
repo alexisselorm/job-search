b514086d2a8d2ca9cc43d9200ed9bbdb7078e280
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-11-16 17:18:21 +0000

    Extracted top prompt and clearfilters button out of filters sidebar Added tests for sidebar prompt

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -3,16 +3,7 @@
     class="flex flex-col p-4 bg-white border-r border-solid border-brand-gray-1 w-96"
   >
     <section class="pb-5">
-      <div class="flex flex-row justify between">
-        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
-        <div class="flex items-center text-sm">
-          <ActionButton
-            text="Clear Filters"
-            type="secondary"
-            @click="clearUserJobFilterSelections"
-          />
-        </div>
-      </div>
+      <JobFiltersSidebarPrompt />
       <Accordion header="Skills">
         -->
         <!-- If I don't specify anything here, the fallback text in the Accordion will fire -->
@@ -32,34 +23,20 @@
   </div>
 </template>
 <script lang="ts">
-import ActionButton from "@/components/Shared/ActionButton.vue";
-import { key } from "@/store";
-import { useStore } from "vuex";
 import Accordion from "@/components/Shared/Accordion.vue";
 import JobFiltersSidebarDegrees from "./JobFiltersSidebarDegrees.vue";
 import JobFiltersSidebarJobTypes from "./JobFiltersSidebarJobTypes.vue";
 import JobFiltersSidebarOrganizations from "./JobFiltersSidebarOrganizations.vue";
-
-import { CLEAR_USER_JOB_FILTER_SELECTIONS } from "@/store/constants";
+import JobFiltersSidebarPrompt from "./JobFiltersSidebarPrompt.vue";
 import { defineComponent } from "vue";
 export default defineComponent({
   name: "JobResultsSidebar",
   components: {
-    ActionButton,
     Accordion,
+    JobFiltersSidebarPrompt,
     JobFiltersSidebarDegrees,
     JobFiltersSidebarJobTypes,
     JobFiltersSidebarOrganizations,
-  },
-  setup() {
-    const store = useStore(key);
-
-    const clearUserJobFilterSelections = () => {
-      store.commit(CLEAR_USER_JOB_FILTER_SELECTIONS);
-    };
-    return {
-      clearUserJobFilterSelections,
-    };
   },
 });
 </script>
