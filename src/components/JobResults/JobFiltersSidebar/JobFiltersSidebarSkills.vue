98751cdcfd82bcf41a7e8a46e6fc8f991766e14b
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-11-17 21:42:18 +0000

    Added tests for skills search

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarSkills.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarSkills.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <input
-      v-model.lazy="skillsSearchTerm"
+      v-model.lazy.trim="skillsSearchTerm"
       class="p-3 h-12 border border-solid border-brand-gray-1 shadow-gray rounded w-full text-base"
       placeholder="Computer promgramming, Finance degree"
+      data-test="skills-search-input"
     />
   </div>
 </template>
